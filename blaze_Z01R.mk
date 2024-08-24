6ab05334abb5136635573be513aa610ebdd0d650
Thunderstorm1708 <89205550+Thunderstorm1708@users.noreply.github.com>
2024-08-24 17:35:36 +0530

    Z01R: Bringup ProjectBlaze

--- blaze_Z01R.mk
+++ blaze_Z01R.mk
@@ -22,14 +22,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Inherit from Z01R device
 $(call inherit-product, device/asus/Z01R/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common blaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+# Blaze
+BLAZE_MAINTAINER := Pankaj
+BLAZE_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := lineage_Z01R
+PRODUCT_NAME := blaze_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
